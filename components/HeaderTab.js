acc1a575676df7d4f46fab41326e0aae0a4e10b2
suthicha <suthicha@outlook.com>
2022-05-14 18:45:50 +0700

    init project

---
+++ components/HeaderTab.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function HeaderTab() {
+  return (
+    <View>
+      <Text>Delivery</Text>
+    </View>
+  );
+}
+
+const HeaderButton = () => <Text>Delivery</Text>;
---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from 'react-native';
+import React from 'react';
+import HeaderTab from '../components/HeaderTab';
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <HeaderTab />
+    </SafeAreaView>
+  );
+}
