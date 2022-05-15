5e367d27bed631c68f0c68cc77d3d9df6330782b
suthicha <suthicha@outlook.com>
2022-05-15 15:04:43 +0700

    add google search feature

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,19 @@
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, ScrollView } from 'react-native';
 import React from 'react';
 import HeaderTab from '../components/HeaderTab';
+import SearchBar from '../components/SearchBar';
+import Categories from '../components/Categories';
 
 export default function Home() {
   return (
-    <SafeAreaView>
-      <HeaderTab />
+    <SafeAreaView style={{ backgroundColor: '#eee', flex: 1 }}>
+      <View style={{ backgroundColor: '#fff', padding: 15 }}>
+        <HeaderTab />
+        <SearchBar />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+      </ScrollView>
     </SafeAreaView>
   );
 }
